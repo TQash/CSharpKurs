b57ed205f9d5f1e19ba54190afb2b179df66dcd9
Thabit Qash <qash77@outlook.com>
2018-06-12 03:44:40 +0200

    komment

---
+++ Variablen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variablen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myValue = 5;
+            Console.WriteLine(myValue);
+
+        }
+    }
+}
